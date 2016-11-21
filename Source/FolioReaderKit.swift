4a5bbb274c71090cab3678a7e45578da73a6d6a1
Daniel F. Sampaio <dan.faria.sampaio@gmail.com>
2016-11-21 11:24:08 -0200

    Adds delay() helper function

--- Source/FolioReaderKit.swift
+++ Source/FolioReaderKit.swift
@@ -266,6 +266,18 @@ extension ScrollDirection {
     }
 }
 
+/**
+ Delay function
+ From: http://stackoverflow.com/a/24318861/517707
+ 
+ - parameter delay:   Delay in seconds
+ - parameter closure: Closure
+ */
+func delay(_ delay:Double, closure:@escaping ()->()) {
+    DispatchQueue.main.asyncAfter(
+        deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure)
+}
+
 
 // MARK: - Extensions
 
